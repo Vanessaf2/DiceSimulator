f9f61bab4c0990002d002aacfe70562a0ee73680
Vanessa Flores <145719478+Vanessaf2@users.noreply.github.com>
2025-07-21 03:00:32 -0500

    module-info.java

--- src/main/java/dicesimulator/module-info.java
+++ src/main/java/dicesimulator/module-info.java
@@ -1,7 +1,7 @@
 module dicesimulator {
     requires javafx.controls;
     requires javafx.fxml;
-    requires transitive javafx.graphics; // This line fixes the error you saw
+    requires transitive javafx.graphics;
 
     opens dicesimulator to javafx.fxml;
     exports dicesimulator;
